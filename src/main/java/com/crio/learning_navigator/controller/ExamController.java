75a116ef84107a138e9775ae434287a3b0803e97
Nandini Chauhan <hackergwen@proton.me>
2024-07-15 23:47:30 +0530

    Add controller layer

--- src/main/java/com/crio/learning_navigator/controller/ExamController.java
+++ src/main/java/com/crio/learning_navigator/controller/ExamController.java
@@ -1,2 +1,40 @@
-package com.crio.learning_navigator.controller;public class ExamController {
+package com.crio.learning_navigator.controller;
+
+import com.crio.learning_navigator.model.Exam;
+import com.crio.learning_navigator.service.ExamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exams")
+public class ExamController {
+
+    private final ExamService examService;
+
+    @Autowired
+    public ExamController(ExamService examService) {
+        this.examService = examService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Exam> createExam(@RequestBody Exam exam) {
+        Exam createdExam = examService.createExam(exam);
+        return new ResponseEntity<>(createdExam, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Exam>> getAllExams() {
+        List<Exam> exams = examService.getAllExams();
+        return ResponseEntity.ok(exams);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Exam> getExamById(@PathVariable Long id) {
+        Exam exam = examService.getExamById(id);
+        return ResponseEntity.ok(exam);
+    }
 }
--- src/main/java/com/crio/learning_navigator/controller/StudentController.java
+++ src/main/java/com/crio/learning_navigator/controller/StudentController.java
@@ -1,2 +1,63 @@
-package com.crio.learning_navigator.controller;public class StudentController {
+package com.crio.learning_navigator.controller;
+
+import com.crio.learning_navigator.model.Student;
+import com.crio.learning_navigator.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student createdStudent = studentService.createStudent(student);
+        return new ResponseEntity<>(createdStudent, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+        return ResponseEntity.ok(studentService.getStudentById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.getAllStudents();
+        return ResponseEntity.ok(students);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student studentDetails) {
+        Student updatedStudent = studentService.updateStudent(id, studentDetails);
+        return ResponseEntity.ok(updatedStudent);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id) {
+        String response = studentService.deleteStudent(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{registrationId}/enroll/{subjectId}")
+    public ResponseEntity<Student> enrollStudentInSubject(@PathVariable Long registrationId, @PathVariable Long subjectId) {
+        Student enrolledStudent = studentService.enrollStudentInSubject(registrationId, subjectId);
+        return ResponseEntity.ok(enrolledStudent);
+    }
+
+    @PostMapping("/{registrationId}/register/{examId}")
+    public ResponseEntity<Student> registerStudentForExam(@PathVariable Long registrationId, @PathVariable Long examId) {
+        Student registeredStudent = studentService.registerStudentForExam(registrationId, examId);
+        return ResponseEntity.ok(registeredStudent);
+    }
 }
